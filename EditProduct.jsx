251cd00d905ab68f54212df7c75103e7427dc745
slackerhacker21 <jjenni19@asu.edu>
2025-03-26 01:14:49 -0400

    Create EditProduct.jsx

---
+++ EditProduct.jsx
@@ -0,0 +1,41 @@
+<Form.Control
+            type="number"
+            name="price"
+            value={formData.price}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formDescription" className="mb-3">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            as="textarea"
+            rows={3}
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formCategory" className="mb-3">
+          <Form.Label>Category</Form.Label>
+          <Form.Control
+            type="text"
+            name="category"
+            value={formData.category}
+            onChange={handleChange}
+            required
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" disabled={updating}>
+          {updating ? <Spinner animation="border" size="sm" /> : 'Update Product'}
+        </Button>
+      </Form>
+    </>
+  );
+}
+
+export default EditProduct;
