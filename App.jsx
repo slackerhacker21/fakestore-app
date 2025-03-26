cc9da33078db53e4230eb6ed0ff4d5279080d5ab
slackerhacker21 <jjenni19@asu.edu>
2025-03-26 01:13:17 -0400

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+import AddProduct from './pages/AddProduct';
+import EditProduct from './pages/EditProduct';
+import CustomNavbar from './components/CustomNavbar';
+
+function App() {
+  return (
+    <>
+      <CustomNavbar />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/add-product" element={<AddProduct />} />
+          <Route path="/edit-product/:id" element={<EditProduct />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
